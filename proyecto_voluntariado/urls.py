210a625c694b8be4d33231829b3a820307d997f5
Javier Perez <javier.alejandro92@gmail.com>
2012-09-10 11:22:51 -0600

    Commit inicial

---
+++ proyecto_voluntariado/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls.defaults import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'proyecto_voluntariado.views.home', name='home'),
+    # url(r'^proyecto_voluntariado/', include('proyecto_voluntariado.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
